0666e1c2fdf2ede4b4b0801f1a1b55f02293a6ca
mariaecs545 <madudasilva050405@gmail.com>
2026-06-06 18:31:52 -0300

    integracao do postgre

--- dags/etl_enderecos_aracaju.py
+++ dags/etl_enderecos_aracaju.py
@@ -2,10 +2,27 @@ from airflow import DAG # type: ignore
 from airflow.operators.python import PythonOperator # type: ignore
 from datetime import datetime
 import pandas as pd
+from sqlalchemy import create_engine
 
 def processar_dados():
-    df = pd.read_csv('https://docs.google.com/uc?export=download&id=1SrT78fHBCDJx9vox7tlcpq_kVccmjCj1', sep=';')
-    df_aracaju = df[df['city'] == 'ARACAJU']
+    caminho_arquivo = '/opt/airflow/dags/dados_processo_seletivo.csv'
+    df = pd.read_csv(caminho_arquivo, sep=None, engine='python')
+    df_aracaju = df[df['city'].astype(str).str.upper() == 'ARACAJU'] # filtra aracaju
+
+    # conexao PostgreSQL
+    engine = create_engine(
+        "postgresql+psycopg2://admin:admin@postgres:5432/banco_etl"
+    )
+
+    # salva na tabela
+    df_aracaju.to_sql(
+        "enderecos_aracaju",
+        engine,
+        if_exists="replace",
+        index=False
+    )
+
+    print(f"{len(df_aracaju)} registros inseridos.")
 
 with DAG(
     dag_id = 'dag_filtro_aracaju',
